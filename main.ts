7af1fab67f0fcbbd41300a4e3744ba98037086e3
anton <sanghoemail@gmail.com>
2025-02-23 21:03:26 +0900

    서보가 동시에 움직이도록 수정

--- main.ts
+++ main.ts
@@ -1,36 +1,42 @@
 input.onSound(DetectedSound.Loud, function () {
-    basic.clearScreen()
-    for (let index = 0; index < 3; index++) {
+    // 각 서보의 동작 각도를 배열로 정의
+    servo1_angles = [
+    0,
+    45,
+    45,
+    90
+    ]
+    servo2_angles = [
+    150,
+    150,
+    120,
+    90
+    ]
+    for (let index = 0; index < 2; index++) {
+        basic.clearScreen()
+        // 초기 화면 설정
         basic.showLeds(`
-            . . . . .
+            # # # # #
             # # # # #
             # . . . #
-            . # # # .
-            . . . . .
+            # # # # #
+            # # # # #
             `)
-        basic.clearScreen()
-        basic.pause(100)
-        servos.P2.setAngle(135)
-        servos.P1.setAngle(45)
-        basic.pause(2000)
-        servos.P2.setAngle(45)
-        servos.P1.setAngle(135)
-        basic.pause(2000)
-        servos.P2.setAngle(135)
-        servos.P1.setAngle(45)
-        basic.pause(2000)
+        for (let i = 0; i <= servo1_angles.length - 1; i++) {
+            // 두 서보 동시 설정
+            servos.P1.setAngle(servo1_angles[i])
+            servos.P2.setAngle(servo2_angles[i])
+            basic.pause(200)
+            basic.clearScreen()
+        }
     }
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
 })
 input.onButtonPressed(Button.A, function () {
 	
 })
+let servo2_angles: number[] = []
+let servo1_angles: number[] = []
+// 초기 화면 설정
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -38,8 +44,12 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
-servos.P2.setAngle(90)
-servos.P1.setAngle(45)
+// 서보 초기 설정
+servos.P1.setPulse(2500)
+servos.P2.setPulse(2500)
+// 초기 각도 설정
+servos.P2.setAngle(95)
+servos.P1.setAngle(90)
 basic.forever(function () {
 	
 })
