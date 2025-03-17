f1780a647fd7e92c28ab94044422935df5ec47a5
anton <sanghoemail@gmail.com>
2025-03-17 22:29:36 +0900

    소리크기의 값을 정해서 동작하도로 수정, 소리가 안날 때의 초기값 등록

--- main.ts
+++ main.ts
@@ -1,28 +1,5 @@
 input.onSound(DetectedSound.Loud, function () {
-    // 각 서보의 동작 각도를 배열로 정의
-    servo1_angles = [
-    0,
-    45,
-    45,
-    90
-    ]
-    servo2_angles = [
-    150,
-    150,
-    120,
-    90
-    ]
-    for (let index = 0; index < 2; index++) {
-        basic.clearScreen()
-        basic.showIcon(IconNames.Heart)
-        for (let i = 0; i <= servo1_angles.length - 1; i++) {
-            // 두 서보 동시 설정
-            servos.P1.setAngle(servo1_angles[i])
-            servos.P2.setAngle(servo2_angles[i])
-            basic.pause(200)
-            basic.clearScreen()
-        }
-    }
+	
 })
 let servo2_angles: number[] = []
 let servo1_angles: number[] = []
@@ -33,3 +10,34 @@ servos.P2.setPulse(2500)
 // 초기 각도 설정
 servos.P2.setAngle(95)
 servos.P1.setAngle(90)
+basic.forever(function () {
+    if (input.soundLevel() > 120) {
+        servo1_angles = [
+        0,
+        45,
+        45,
+        90
+        ]
+        servo2_angles = [
+        150,
+        150,
+        120,
+        90
+        ]
+        for (let index = 0; index < 2; index++) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Heart)
+            for (let i = 0; i <= servo1_angles.length - 1; i++) {
+                // 두 서보 동시 설정
+                servos.P1.setAngle(servo1_angles[i])
+                servos.P2.setAngle(servo2_angles[i])
+                basic.pause(200)
+                basic.clearScreen()
+            }
+        }
+    } else {
+        // 초기 각도 설정
+        servos.P2.setAngle(95)
+        servos.P1.setAngle(90)
+    }
+})
